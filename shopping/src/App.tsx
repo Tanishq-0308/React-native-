6b2d4c6098f557c2f10452349b420b240bf60fb4
Tanishq <sharmatanishq0308@gmail.com>
2025-01-20 16:17:46 +0530

    shopping page added

---
+++ shopping/src/App.tsx
@@ -0,0 +1,35 @@
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import Home from './screens/Home';
+import Details from './screens/Details';
+
+export type RootParamList= {
+  Home:undefined;
+  Details:{Product: Product}
+};
+
+const Stack= createNativeStackNavigator<RootParamList>()
+function App(): React.JSX.Element {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name='Home'
+          component={Home}
+          options={{
+            title:'Trending Products'
+          }}
+        />
+        <Stack.Screen
+          name='Details'
+          component={Details}
+          options={{
+            title:'Product Details'
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+export default App;
---
+++ shopping/src/index.d.ts
@@ -0,0 +1,11 @@
+interface Product{
+    id: number;
+    name: string;
+    imageUrl: string;
+    originalPrice: number;
+    discountPrice: number;
+    offerPercentage: number;
+    rating: number;
+    ratingCount: number;
+    tags: string[];
+}
